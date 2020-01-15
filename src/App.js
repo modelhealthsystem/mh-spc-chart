6d38535c0be19e175d59a1a7a3317c767bd9e80a
Andrew Campbell <andrew.campbell@paconsulting.com>
2020-01-15 13:53:20 +0000

    Initial Commit - stable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import SpcChartComponent from './spcChart';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class SpcChart extends Component {
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <h1>SPC Chart Component</h1>
+        <div className="content-container">
+          <SpcChartComponent />
+        </div>
+      </div>
+    );
+  }
+}
